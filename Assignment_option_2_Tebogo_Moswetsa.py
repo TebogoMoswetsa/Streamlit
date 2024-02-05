c88ddd7e4b2cf57ddb9558ca62eb79588c565915
TebogoMoswetsa <158013441+TebogoMoswetsa@users.noreply.github.com>
2024-02-06 00:01:07 +0200

    Update Assignment_option_2_Tebogo_Moswetsa.py

--- Assignment_option_2_Tebogo_Moswetsa.py
+++ Assignment_option_2_Tebogo_Moswetsa.py
@@ -6,7 +6,6 @@ Created on Mon Feb  5 22:35:10 2024
 """
 import pandas as pd
 import streamlit as st
-import matplotlib.pyplot as plt
 
 
 #importing the data from an csv file
@@ -20,10 +19,5 @@ yAxis = df['Age']
 xAxis = df['Country']
 
 
-#plotting a graph with age
-fig = plt.figure() 
-plt.plot(yAxis) 
-
-st.pyplot(fig)
-
 st.line_chart(df[["Age", "Country"]])
+
